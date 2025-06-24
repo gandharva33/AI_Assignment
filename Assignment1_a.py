426810e1f15efeebb0d322680a4ae11a069c8d7a
Gandharva Manoj Vaidya <gandharvavaidya11@gmail.com>
2025-06-24 14:47:59 +0530

    Updated assignments

--- Assignment1_a.py
+++ Assignment1_a.py
@@ -1,27 +1,32 @@
+# Program to print the number of prime numbers given by the user
 try:
-    n = int(input("How many prime numbers do you want? "))
+    n = int(input("How many prime numbers do you want: "))  # Ask the user input for the number of prime numbers to be printed
 
     if n <= 0:
-        print("Please enter a number greater than 0.")
-    else:
-        count = 0       
-        num = 2          
+        print("Please enter a number greater than 0.")  # Checks if the number is less than or equal to 0
+    else:               # Continues the loop if the number is greater than 0
+        count = 0
+        num = 2  
 
-        print("Prime numbers are:")
+        print("Prime numbers are:")  # Prints the 'Prime numbers are:
 
-        while count < n:
-            
-            prime = True
-            for i in range(2, num):
+        while count < n:  # Loop to find and print the first n prime numbers
+
+            prime = True  # Assuming the number is prime initially
+
+            for i in range(2, num):  # Loop to check if num is a prime number
                 if num % i == 0:
                     prime = False
 
-            if prime:
+            if prime:  # If num is prime then it will be printed
                 print(num)
                 count += 1
-            num += 1  
 
-        print("Total prime numbers printed:", count)
+            num += 1
+
+        print("Total prime numbers printed:", count)  # Count of prime numbers to be printed.
 
 except ValueError:
-    print("Invalid input! Please enter a whole number like 5 or 10.")
+    print("Invalid input! Please enter a whole number like 5 or 10.")  # Exception handling for invalid input.
+
+
--- Assignment1_b.py
+++ Assignment1_b.py
@@ -1,17 +1,19 @@
-n = int(input("Enter a number:"))
+n = int(input("Enter a number:")) # Ask the user to enter a number.
 
+# To check if the number is less than or equal to 1
 if n <= 1:
-    print("The number should be greater than 1.")
+    print("The number should be greater than 1.") # Prime numbers are greater than 1
 
 else:
-    c = False
+    c = False # This will help us know if number is composite
 
-    for i in range(2,n):
-        if n%i == 0:
-            c = True    
+    for i in range(2,n): # Loop through numbers from 2 to n
+        if n%i == 0: 
+            c = True     # Continue the loop if n is divisible by i
 
+# If c is true then number is composite
 if c:
-    print("The given number is a composite number.")
+    print("The given number is a composite number.") 
 
 else:
-    print("The given number is a prime number.")                
+    print("The given number is a prime number.") # If it isnt divisible then it means it is a prime number.              
